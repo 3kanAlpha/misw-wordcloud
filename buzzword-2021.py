01267cfd4e3ff283f795b9b1c35d4a09b7b5c478
m4gnett <39447635+3kanAlpha@users.noreply.github.com>
2021-11-25 18:02:57 +0900

    仮完成

--- buzzword-2021.py
+++ buzzword-2021.py
@@ -5,6 +5,10 @@ import MeCab
 from wordcloud import WordCloud
 import re
 
+import json
+import time
+import collections
+
 from dotenv import load_dotenv
 
 load_dotenv('.env')
@@ -12,13 +16,19 @@ load_dotenv('.env')
 # SourceHanSans
 FONT_PATH = os.environ.get("FONT_PATH")
 TEXT_NAME = "rashomon"
-TWEETS_PATH = "tweets.json"
+
+TWEETS_DIR = "tweets/"
+TWEETS_JSON_PATH = TWEETS_DIR + "tweets.json"
+
+USER_IDS_PATH = "user_ids.json"
+
+WORDCLOUD_TITLE = "misw-buzzword"
 
 def generate_wordcloud(text):
     wc = WordCloud(background_color="white", font_path=FONT_PATH, max_words=1000, max_font_size=300, width=800, height=600)
     wc.generate(text)
 
-    wc.to_file("{}.png".format(TEXT_NAME))
+    wc.to_file("{}.png".format(WORDCLOUD_TITLE))
 
 def list_to_string(l):
     return ' '.join(l)
@@ -41,11 +51,62 @@ def get_words_from_text(text):
     
     return words
 
+# 本質的でない単語を除く
+def remove_words(list):
+    for s in list:
+        if s.startswith("https://") or s.startswith("http://"):
+            list.remove(s)
+        
+        if s == "前日比":
+            list.remove(s)
+        elif s == "https":
+            list.remove(s)
+        elif s == "t":
+            list.remove(s)
+        elif s == "co":
+            list.remove(s)
+
 def main():
-    txt = open("127_ruby_150/{}.txt".format(TEXT_NAME), encoding="utf8").read()
-    words = get_words_from_text(txt)
-    words_str = list_to_string(words)
+    user_ids_json = open(USER_IDS_PATH, 'r', encoding="utf-8")
+    user_ids = json.load(user_ids_json)
+    user_ids_json.close()
+
+    tweets_json = open(TWEETS_JSON_PATH, 'r', encoding="utf-8")
+    tweets = json.load(tweets_json)
+    tweets_json.close()
+
+    all_tweets = tweets["tweets"]
+
+    words_to_wc = []
+
+    start_time = time.perf_counter()
+
+    for user in user_ids["ids"]:
+        if user["name"] in all_tweets:
+            print("Loading {}'s tweets ... ({})".format(user["name"], len(all_tweets[user["name"]])))
+            for tweet in all_tweets[user["name"]]:
+                words_to_wc.extend(get_words_from_text(tweet["tweet"]))
+
+    # time_1 = time.perf_counter()
+
+    # counter = collections.Counter(words_to_wc)
+
+    print("Removing unrelated words ...")
+    remove_words(words_to_wc)
+
+    # time_2 = time.perf_counter()
+
+    print("Words to string ...")
+    words_str = list_to_string(words_to_wc)
+
+    # time_3 = time.perf_counter()
+
+    print("Generating wordcoud ...")
     generate_wordcloud(words_str)
+
+    time_end = time.perf_counter()
+
+    print("Done! ({} [sec])".format(time_end - start_time))
 
 if __name__ == '__main__':
     main()
